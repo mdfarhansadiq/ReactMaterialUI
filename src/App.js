bcf7eff6221c90a8af55ee8a42c952f6081d1d1d
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2021-01-17 14:08:51 +0600

    React Material UI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import SaveIcon from '@material-ui/icons/Save'
-import {Delete} from "@material-ui/icons";
+import DeleteIcon from '@material-ui/icons/Delete';
 function App() {
 
   return (
@@ -15,7 +15,7 @@ function App() {
                   startIcon={<SaveIcon/>}
                   size="large" variant="contained" color="primary" >Save</Button>
               <Button
-                  endIcon={<SaveIcon/>}
+                  endIcon={<DeleteIcon/>}
                   size="large" variant="contained" color="secondary" >Discard</Button>
           </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
