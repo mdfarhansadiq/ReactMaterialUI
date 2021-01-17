1334771aa7f2c8e9fcabb92782280131ed38c512
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2021-01-17 14:02:26 +0600

    React Material UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import SaveIcon from '@material-ui/icons/Save'
+import {Delete} from "@material-ui/icons";
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
+          <ButtonGroup>
+              <Button
+                  startIcon={<SaveIcon/>}
+                  size="large" variant="contained" color="primary" >Save</Button>
+              <Button
+                  endIcon={<SaveIcon/>}
+                  size="large" variant="contained" color="secondary" >Discard</Button>
+          </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
